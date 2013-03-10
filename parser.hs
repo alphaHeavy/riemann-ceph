f6a118a8a241c3a0778cd54e5d0aed2e97e84c45
Nathan Howell <nhowell@alphaheavy.com>
2013-03-09 18:25:07 -0800

    Switching to riemann-hs

--- parser.hs
+++ parser.hs
@@ -16,14 +16,10 @@ import Data.Conduit.Attoparsec (conduitParser)
 import qualified Data.Conduit.List as Cl
 import Data.Conduit.Process (proc, sourceProcess)
 import qualified Data.Conduit.Network.UDP as UDP
-import Data.Int
-import Data.Monoid
-import Data.ProtocolBuffers
+import Data.ProtocolBuffers (Signed(..), encodeMessage)
 import Data.Serialize (runPut)
-import Data.TypeLevel hiding (Bool)
 import Data.Text (Text)
 import qualified Data.Text as T
-import GHC.Generics (Generic)
 import qualified Data.HashMap.Strict as HashMap
 import qualified Data.Vector as Vector
 import Data.Time
@@ -31,30 +27,14 @@ import Data.Time.Clock.POSIX
 import System.Locale (defaultTimeLocale)
 import Network.Socket
 
-{-
-failMaybe :: a -> Maybe b -> Either a b
-failMaybe _ (Just v) = Right v
-failMaybe x _        = Left x
-
-foo :: Aeson.Value -> Either String RiemannMsg
-foo (Aeson.Object obj) = do
-  ver <- failMaybe "version"   $ HashMap.lookup "version" obj
-  Aeson.String ts  <- failMaybe "timestamp" $ HashMap.lookup "timestamp" obj
-  Aeson.Object healthObj       <- failMaybe "health" $ HashMap.lookup "health" obj
-  Aeson.Array healthSummaryArr <- failMaybe "summary" $ HashMap.lookup "summary" healthObj
-  Aeson.String healthStatus    <- failMaybe "overall_status" $ HashMap.lookup "overall_status" healthObj
-
-
-  case Vector.toList healthSummaryArr of
-    [Aeson.Object healthSummaryObj] -> do
-      Aeson.String healthSummary <- failMaybe "overall_status/summary" $ HashMap.lookup "summary" healthSummaryObj
-      return undefined
--}
+import Data.Default
+import Control.Lens
+import qualified Network.Monitoring.Riemann as Riemann
 
 parseJSONTime :: Text -> Maybe UTCTime
 parseJSONTime = parseTime defaultTimeLocale "%Y-%m-%d %H:%M:%S%Q" . T.unpack
 
-pgmapEvents :: Aeson.Object -> [RiemannEvent]
+pgmapEvents :: Aeson.Object -> [Riemann.Event]
 pgmapEvents obj
   | Just (Aeson.String ts) <- HashMap.lookup "stamp" obj
   , Just ts' <- parseJSONTime ts
@@ -72,32 +52,24 @@ pgmapEvents obj
   , Just (Aeson.Number kbUsed)      <- HashMap.lookup "kb_used" osdStatsSum
   , Just (Aeson.Number kbTotal)     <- HashMap.lookup "kb" osdStatsSum
 
-  = let et = RiemannEvent
-          { eventTime         = putField . Just . floor $ utcTimeToPOSIXSeconds ts'
-          , eventState        = mempty
-          , eventService      = mempty
-          , eventHost         = putField $ Just "ceph"
-          , eventDescription  = mempty
-          , eventTags         = mempty
-          , eventTtl          = mempty
-          , eventAttributes   = mempty
-          , eventMetricSInt64 = mempty
-          , eventMetricD      = mempty
-          , eventMetricF      = mempty
-          }
+  = let et = def & Riemann.host .~ Just "ceph"
+                 & Riemann.time .~ Just (floor $ utcTimeToPOSIXSeconds ts')
 
-    in [ et{eventService = putField $ Just "object clones", eventMetricF = putField . Just . realToFrac $ clones/objects}
-       , et{eventService = putField $ Just "copies", eventMetricF = putField . Just . realToFrac $ copies/objects}
-       , et{eventService = putField $ Just "object missing", eventMetricF = putField . Just . realToFrac $ missing/objects}
-       , et{eventService = putField $ Just "object degraded", eventMetricF = putField . Just . realToFrac $ degraded/objects}
-       , et{eventService = putField $ Just "object unfound", eventMetricF = putField . Just . realToFrac $ unfound/objects}
-       , et{eventService = putField $ Just "object recovered", eventMetricF = putField . Just . realToFrac $ recovered/objects}
-       , et{eventService = putField $ Just "fs used", eventMetricF = putField . Just . realToFrac $ kbUsed/kbTotal}
+        realToFloat :: Real a => a -> Maybe Double
+        realToFloat = Just . realToFrac
+
+    in [ et & Riemann.service .~ Just "object clones"    & Riemann.metric .~ realToFloat (clones/objects)
+       , et & Riemann.service .~ Just "copies"           & Riemann.metric .~ realToFloat (copies/objects)
+       , et & Riemann.service .~ Just "object missing"   & Riemann.metric .~ realToFloat (missing/objects)
+       , et & Riemann.service .~ Just "object degraded"  & Riemann.metric .~ realToFloat (degraded/objects)
+       , et & Riemann.service .~ Just "object unfound"   & Riemann.metric .~ realToFloat (unfound/objects)
+       , et & Riemann.service .~ Just "object recovered" & Riemann.metric .~ realToFloat (recovered/objects)
+       , et & Riemann.service .~ Just "fs used"          & Riemann.metric .~ realToFloat (kbUsed/kbTotal)
        ]
 
   | otherwise = []
 
-valueToMessage :: (a, Aeson.Value) -> RiemannMsg
+valueToMessage :: (a, Aeson.Value) -> Riemann.Msg
 valueToMessage (_, Aeson.Object obj)
   | Just (Aeson.String ver) <- HashMap.lookup "version" obj
   , Just (Aeson.Object healthObj) <- HashMap.lookup "health" obj
@@ -110,37 +82,18 @@ valueToMessage (_, Aeson.Object obj)
   , Just ts' <- parseJSONTime ts
   , Just (Aeson.Object pgmap) <- HashMap.lookup "pgmap" obj
 
-  = let healthy = case healthStatus of
-          "HEALTH_OK"   -> Just True
-          "HEALTH_WARN" -> Just False
-          "HEALTH_ERR"  -> Just False
-          _             -> Nothing
+  = let evt = def
+          & Riemann.time         .~ Just (Signed . floor $ utcTimeToPOSIXSeconds ts')
+          & Riemann.state        .~ Just healthStatus
+          & Riemann.service      .~ Just "health"
+          & Riemann.host         .~ Just "ceph"
+          & Riemann.description  .~ Just healthSummary
 
-        evt = RiemannEvent
-          { eventTime         = putField . Just . floor $ utcTimeToPOSIXSeconds ts'
-          , eventState        = putField $ Just healthStatus
-          , eventService      = putField $ Just "health"
-          , eventHost         = putField $ Just "ceph"
-          , eventDescription  = putField $ Just healthSummary
-          , eventTags         = mempty
-          , eventTtl          = mempty
-          , eventAttributes   = mempty
-          , eventMetricSInt64 = mempty
-          , eventMetricD      = mempty
-          , eventMetricF      = mempty
-          }
-
-    in RiemannMsg
-         { msgOk     = putField healthy
-         , msgError  = mempty
-         , msgStates = mempty
-         , msgQuery  = mempty
-         , msgEvents = putField (evt:pgmapEvents pgmap)
-         }
+    in def & Riemann.events .~ (evt:pgmapEvents pgmap)
 
   | otherwise = undefined
 
-cephMon :: MonadResource m => GSource m RiemannMsg
+cephMon :: MonadResource m => GSource m Riemann.Msg
 cephMon =
   sourceProcess (proc "ceph" ["report"])
     >+> conduitToPipe (conduitParser cephJson)
@@ -160,55 +113,3 @@ main = do
     >+> Cl.map (runPut . encodeMessage)
     >+> Cl.map (`UDP.Message` addr)
     >+> UDP.sinkToSocket s
-
-data RiemannState = RiemannState
-  { stateTime        :: Optional D1 (Value Int64)
-  , stateState       :: Optional D2 (Value Text)
-  , stateService     :: Optional D3 (Value Text)
-  , stateHost        :: Optional D4 (Value Text)
-  , stateDescription :: Optional D5 (Value Text)
-  , stateOnce        :: Optional D6 (Value Bool)
-  , stateTags        :: Repeated D7 (Value Text)
-  , stateTtl         :: Optional D8 (Value Float)
-  } deriving (Generic, Show)
-
-instance Encode RiemannState
-
-data RiemannEvent = RiemannEvent
-  { eventTime         :: Optional D1 (Value Int64)
-  , eventState        :: Optional D2 (Value Text)
-  , eventService      :: Optional D3 (Value Text)
-  , eventHost         :: Optional D4 (Value Text)
-  , eventDescription  :: Optional D5 (Value Text)
-  , eventTags         :: Repeated D7 (Value Text)
-  , eventTtl          :: Optional D7 (Value Float)
-  , eventAttributes   :: Repeated D7 (Message RiemannAttribute)
-  , eventMetricSInt64 :: Optional D13 (Value (Signed Int64))
-  , eventMetricD      :: Optional D14 (Value Double)
-  , eventMetricF      :: Optional D15 (Value Float)
-  } deriving (Generic, Show)
-
-instance Encode RiemannEvent
-
-data RiemannQuery = RiemannQuery
-  { queryString :: Optional D1 (Value Text)
-  } deriving (Generic, Show)
-
-instance Encode RiemannQuery
-
-data RiemannMsg = RiemannMsg
-  { msgOk     :: Optional D2 (Value Bool)
-  , msgError  :: Optional D3 (Value Text)
-  , msgStates :: Repeated D4 (Message RiemannState)
-  , msgQuery  :: Optional D5 (Message RiemannQuery)
-  , msgEvents :: Repeated D6 (Message RiemannEvent)
-  } deriving (Generic, Show)
-
-instance Encode RiemannMsg
-
-data RiemannAttribute = RiemannAttribute
-  { attributeKey   :: Required D1 (Value Text)
-  , attributeValue :: Optional D2 (Value Text)
-  } deriving (Generic, Show)
-
-instance Encode RiemannAttribute
