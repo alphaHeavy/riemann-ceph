9316728750606b8944ca0e9dc62bea9e25148ec2
Nathan Howell <nhowell@alphaheavy.com>
2014-07-12 15:38:42 -0700

    Upgrade to protobuf-0.2, requires GHC 7.8.x or newer

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-git clone https://github.com/tel/riemann-hs.git
+git clone https://github.com/alphaHeavy/riemann-hs.git -b protobuf-0.2
 cabal sandbox init
 cabal sandbox add-source riemann-hs
 cabal install --dependencies-only
